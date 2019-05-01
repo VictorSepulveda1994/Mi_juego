143d5a809032ed55e13f0b9e1e17f2a053e6ed8d
vikgamer25 <victorsepulveda1994@gmail.com>
2019-05-01 13:24:59 -0300

    Hoja de Sprites

---
+++ Mi_Juego/src/graficos/HojaSprites.java
@@ -0,0 +1,34 @@
+package graficos;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Victor Sepulveda
+ */
+public class HojaSprites {
+    private final int ancho;
+    private final int alto;
+    public final int[] pixeles;
+    
+    //Constructor de "HojaSprites"
+    public HojaSprites(final String ruta, final int ancho, final int alto){
+        this.ancho = ancho;
+        this.alto = alto;
+        
+        this.pixeles = new int[ancho * alto];
+        
+        //Obtención de los valores de cada pixel en la "HojaSprites"
+        BufferedImage imagen;
+        try {
+            imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
+            imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+        } catch (IOException ex) {
+            Logger.getLogger(HojaSprites.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
